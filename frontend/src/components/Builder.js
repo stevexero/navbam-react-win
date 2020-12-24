0f22c3beac24aa05229fbd5c97991a9e3874c28a
steve <steveanthony999@gmail.com>
2020-12-24 01:48:36 -0800

    added styling to AccordianItemHeading component

--- frontend/src/components/Builder.js
+++ frontend/src/components/Builder.js
@@ -1,4 +1,11 @@
 import styled from 'styled-components'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
 
 const StyledBuilder = styled.section`
     width: 300px;
@@ -7,10 +14,30 @@ const StyledBuilder = styled.section`
     position: fixed;
 `
 
+const StyledAccordianItemHeading = styled(AccordionItemHeading)`
+    &:hover {
+        cursor: pointer;
+    }
+`
+
 const Builder = () => {
     return (
         <StyledBuilder>
-            test
+            <Accordion allowZeroExpanded>
+                {/* NAVBAR BACKGROUND */}
+                {/* NAVBAR BACKGROUND */}
+                {/* NAVBAR BACKGROUND */}
+                <AccordionItem>
+                    <StyledAccordianItemHeading>
+                        <AccordionItemButton>
+                            Navbar Background
+                        </AccordionItemButton>
+                    </StyledAccordianItemHeading>
+                    <AccordionItemPanel>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, reiciendis.</p>
+                    </AccordionItemPanel>
+                </AccordionItem>
+            </Accordion>
         </StyledBuilder>
     )
 }
